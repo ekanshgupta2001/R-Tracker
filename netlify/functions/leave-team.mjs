e84192ba3ebdc26f88c254edfcd456a90325df06
ekanshgupta2001 <ekagupta2001@gmail.com>
2026-04-29 17:45:02 -0600

    Update allowed origins to r-tracker-ftc.netlify.app

--- netlify/functions/leave-team.mjs
+++ netlify/functions/leave-team.mjs
@@ -10,7 +10,7 @@ if (!admin.apps.length) {
 }
 
 const ALLOWED = [
-  'https://r-tracker.netlify.app',
+  'https://r-tracker-ftc.netlify.app',
   'http://localhost:5500',
   'http://127.0.0.1:5500'
 ];
